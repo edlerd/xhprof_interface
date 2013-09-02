32d3946e945f278234c21421c7f11ea701e9abe8
David Edler <de@plista.com>
2013-09-02 17:58:39 +0200

    * allow to filter for calls where a given function is called

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ $handle = opendir($root_dir);
 $pattern = "widgetdata";
 $files = array();
 $limit = isset($_GET['limit']) ? $_GET['limit'] : 20;
+$search = isset($_GET['search']) ? strtolower($_GET['search']) : false;
+$filter = "";
 
 function color($value) {
 	if ($value < 100) {
@@ -25,57 +27,57 @@ if ($handle) {
 }
 krsort($files);
 ?>
-	<script type="text/javascript">
-		var active_runs = [];
-		function toggle_run_link(hash) {
+<script type="text/javascript">
+	var active_runs = [];
+	function toggle_run_link(hash) {
 
-			exists = false;
-			for (i=0; i<active_runs.length; i++) {
-				if (active_runs[i] == hash) {
-					active_runs.splice(i,1);
-					exists = true;
-				}
+		exists = false;
+		for (i=0; i<active_runs.length; i++) {
+			if (active_runs[i] == hash) {
+				active_runs.splice(i,1);
+				exists = true;
 			}
-			if (!exists) {
-				active_runs.push(hash);
-			}
-
-			document.getElementById("aggregated").href='http://<?=gethostname()?>/xhprof/?run='+active_runs.join(',')+'&sort=excl_wt&source=<?=$pattern?>';
+		}
+		if (!exists) {
+			active_runs.push(hash);
 		}
 
-		function select_all() {
-			active_runs=[];
-			var array = document.getElementsByTagName("input");
+		document.getElementById("aggregated").href='http://<?=gethostname()?>/xhprof/?run='+active_runs.join(',')+'&sort=excl_wt&source=<?=$pattern?>';
+	}
 
-			for(var ii = 0; ii < array.length; ii++)
+	function select_all() {
+		active_runs=[];
+		var array = document.getElementsByTagName("input");
+
+		for(var ii = 0; ii < array.length; ii++)
+		{
+			if(array[ii].type == "checkbox")
 			{
-				if(array[ii].type == "checkbox")
+				if(array[ii].className == 'aggregated')
 				{
-					if(array[ii].className == 'aggregated')
-					{
-						array[ii].checked = true;
-						toggle_run_link(array[ii].id)
-					}
+					array[ii].checked = true;
+					toggle_run_link(array[ii].id)
 				}
 			}
 		}
+	}
 
-		function unselect_all() {
+	function unselect_all() {
 
-			var array = document.getElementsByTagName("input");
+		var array = document.getElementsByTagName("input");
 
-			for(var ii = 0; ii < array.length; ii++)
+		for(var ii = 0; ii < array.length; ii++)
+		{
+			if(array[ii].type == "checkbox")
 			{
-				if(array[ii].type == "checkbox")
+				if(array[ii].className == 'aggregated')
 				{
-					if(array[ii].className == 'aggregated')
-					{
-						array[ii].checked = false;
-					}
+					array[ii].checked = false;
 				}
 			}
 		}
-	</script>
+	}
+</script>
 <?php
 echo "<a href='' id='aggregated'>aggregated Profile of selected runs</a>";
 
@@ -86,24 +88,37 @@ echo "<tr>";
 echo "<td><a href='javascript:select_all()'>all</a></a></td>";
 echo "<td>Details</td>";
 echo "<td>Date</td>";
+echo "<td>Match</td>";
 echo "<td>Walltime</td>";
 echo "</tr>";
 
 $i = 0;
 foreach ($files as $file) {
 	$i++;
-	if ($i < $limit) {
-		$path = $root_dir . "/" . $file . "." . $pattern;
-		$xhprof_details=unserialize(file_get_contents($path));
-		$walltime = ($xhprof_details["main()"]["wt"] / 1000) . " ms";
-		$date = date("Y-m-d H:i",filemtime($path));
-	} else {
+
+	if ($i > $limit) {
 		break;
 	}
+
+	$path = $root_dir . "/" . $file . "." . $pattern;
+	$xhprof_details=unserialize(file_get_contents($path));
+	$walltime = ($xhprof_details["main()"]["wt"] / 1000) . " ms";
+	if ($search) {
+		$filter = "";
+		$keys = array_keys($xhprof_details);
+		$keys = implode("",$keys);
+		$keys = strtolower($keys);
+		if (strpos($keys, $search) !== false) {
+			$filter = "found";
+		}
+	}
+	$date = date("Y-m-d H:i",filemtime($path));
+
 	echo "<tr>";
 	echo "<td><input type='checkbox' class='aggregated' onchange='toggle_run_link(\"".$file."\")' id='$file'></input></td>";
 	echo "<td><a href='http://".gethostname()."/xhprof/?run=".$file."&sort=excl_wt&source=$pattern'>".$file."</a></td>";
 	echo "<td>".$date."</td>";
+	echo "<td>".$filter."</td>";
 	echo "<td style='background-color:#".color($walltime)."'>".$walltime."</td>";
 	echo "</tr>";
 }
